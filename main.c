fc0a94ebc4c961e33bef0bef4f22296aec1605d2
amr89mohamed <amr89mohamed@gmail.com>
2024-02-28 06:59:49 -0800

    all is runing

--- main.c
+++ main.c
@@ -1,84 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-
-#define MAX_COMMAND_LENGTH 100
-
+#include "shell.h"
 /*
- * this the dissplay image funcation
- * will display the prompot to the screen
- */void display_prompt(void)
+ * this the main funcation, will print a prompot to
+ * stdio, get the enter by the user by the @get_command
+ * funcation, and store it @command thin pass it to
+ * @argv funcation, which will toknize and return @words
+ * wich will be passed to @excut funcation
+ */int main(void)
 {
-printf("#cisfun$ ");
-}
-
-/*
- * this the main funcation
- */int main(int argc, char *argv[])
-{
-char command[MAX_COMMAND_LENGTH];
-char *args[3];
-
-if (argc > 2)
-{
-printf("Usage: %s [command]\n", argv[0]);
-return (EXIT_FAILURE);
-}
+char *buff = NULL, *command;
+size_t size = 0;
+char **words;
 while (1)
 {
-if (argc == 2)
-{
-snprintf(command, MAX_COMMAND_LENGTH, "/bin/%s", argv[1]);
-args[0] = command;
-args[1] = "-l";
-args[2] = NULL;
-}
-else
-{
-display_prompt();
-if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
-{
-if (feof(stdin))
-{
-printf("\n");
-break;
-}
-else
-{
-perror("Error reading command");
-continue;
-}
-}
-command[strcspn(command, "\n")] = '\0';
-args[0] = command;
-args[1] = NULL;
-}
-pid_t pid = fork();
-if (pid < 0)
-{
-perror("Fork error");
-continue;
-}
-else if (pid == 0)
-{
-if (execve(args[0], args, NULL) == -1)
-{
-perror("Execution error");
-exit(EXIT_FAILURE);
-}
-}
-else
-{
-int status;
-if (waitpid(pid, &status, 0) == -1)
-{
-perror("Wait error");
-exit(EXIT_FAILURE);
-}
-}
+write(1, "cisfun ", 9);
+command = get_command(buff, size);
+words = argv(command);
+excut(words);
 }
 return (0);
 }
-
