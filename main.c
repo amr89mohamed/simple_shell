6f0e941d199543290ef3b177083c3eaada22c781
amr89mohamed <amr89mohamed@gmail.com>
2024-02-28 09:39:42 -0800

    all is wqork

--- main.c
+++ main.c
@@ -10,12 +10,25 @@
 char *buff = NULL, *command;
 size_t size = 0;
 char **words;
+if (isatty(STDIN_FILENO))
 while (1)
 {
 write(1, "$ ", 2);
+command = get_command(buff, size);
+words = argv(command);
+if (strcmp(words[0], "exit") == 0)
+{
+free(command);
+free(words);
+break;            }
+excut(words);
+}
+else
+{
 command = get_command(buff, size);
 words = argv(command);
 excut(words);
 }
 return (0);
 }
+
