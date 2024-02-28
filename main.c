bce852079d29d4d3c83005e3d5f8fc96839ce946
amr89mohamed <amr89mohamed@gmail.com>
2024-02-28 06:48:49 -0800

    new v

--- main.c
+++ main.c
@@ -6,62 +6,79 @@
 
 #define MAX_COMMAND_LENGTH 100
 
-void display_prompt() {
-    printf("#cisfun$ ");
+/*
+ * this the dissplay image funcation
+ * will display the prompot to the screen
+ */void display_prompt(void)
+{
+printf("#cisfun$ ");
 }
 
-int main(int argc, char *argv[]) {
-    char command[MAX_COMMAND_LENGTH];
-    char *args[3]; // Command, argument, and NULL terminator
+/*
+ * this the main funcation
+ */int main(int argc, char *argv[])
+{
+char command[MAX_COMMAND_LENGTH];
+char *args[3];
 
-    if (argc > 2) {
-        printf("Usage: %s [command]\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    while (1) {
-        if (argc == 2) {
-            snprintf(command, MAX_COMMAND_LENGTH, "/bin/%s", argv[1]);
-            args[0] = command;
-            args[1] = "-l";
-            args[2] = NULL;
-        } else {
-            display_prompt();
-            if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
-                if (feof(stdin)) {
-                    printf("\n");
-                    break;
-                } else {
-                    perror("Error reading command");
-                    continue;
-                }
-            }
-            command[strcspn(command, "\n")] = '\0';
-            args[0] = command;
-            args[1] = NULL;
-        }
-
-        pid_t pid = fork();
-
-        if (pid < 0) {
-            perror("Fork error");
-            continue;
-        } else if (pid == 0) {
-            // Child process
-            if (execve(args[0], args, NULL) == -1) {
-                perror("Execution error");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            // Parent process
-            int status;
-            if (waitpid(pid, &status, 0) == -1) {
-                perror("Wait error");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
-
-    return 0;
+if (argc > 2)
+{
+printf("Usage: %s [command]\n", argv[0]);
+return (EXIT_FAILURE);
+}
+while (1)
+{
+if (argc == 2)
+{
+snprintf(command, MAX_COMMAND_LENGTH, "/bin/%s", argv[1]);
+args[0] = command;
+args[1] = "-l";
+args[2] = NULL;
+}
+else
+{
+display_prompt();
+if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+{
+if (feof(stdin))
+{
+printf("\n");
+break;
+}
+else
+{
+perror("Error reading command");
+continue;
+}
+}
+command[strcspn(command, "\n")] = '\0';
+args[0] = command;
+args[1] = NULL;
+}
+pid_t pid = fork();
+if (pid < 0)
+{
+perror("Fork error");
+continue;
+}
+else if (pid == 0)
+{
+if (execve(args[0], args, NULL) == -1)
+{
+perror("Execution error");
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+int status;
+if (waitpid(pid, &status, 0) == -1)
+{
+perror("Wait error");
+exit(EXIT_FAILURE);
+}
+}
+}
+return (0);
 }
 
