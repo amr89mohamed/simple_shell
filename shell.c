e3a1bd5d4421b0d8d4e18a670afdfe2cb3d7b535
amr89moahmed <amr89mohamed@gmail.com>
2024-02-29 05:08:40 -0800

    new update

--- shell.c
+++ shell.c
@@ -5,27 +5,18 @@
  * funcation, and store it @command thin pass it to
  * @argv funcation, which will toknize and return @words
  * wich will be passed to @excut funcation
- */ int main(void)
+ */int main(void)
 {
 char *buff = NULL, *command;
 size_t size = 0;
 char **words;
-char *envp[] = { NULL };
-char *path;
-char *filename;
-char *full_path;
-list_path *path_list;
+char **envp = NULL;
 if (isatty(STDIN_FILENO))
 {
 while (1)
 {
 the_prompt();
 command = get_command(buff, size);
-if (strcmp(command, "\n") == 0)
-{
-free(command);
-continue;
-}
 words = argv(command);
 if (strcmp(words[0], "exit") == 0)
 {
@@ -33,29 +24,7 @@ free(command);
 free(words);
 break;
 }
-words = argv(command);
-if (strcmp(words[0], "exit") == 0)
-{
-exitt(words);
-}
-else if (strcmp(words[0], "env") == 0)
-{
-env(words);
-}
-else if (strcmp(words[0], "_setenv") == 0)
-{
-_setenv(words);
-}
-else if (strcmp(words[0], "_unsetenv") == 0)
-{
-_unsetenv(words);
-}
-else
-{
 excut(words, envp);
-}
-free(command);
-free(words);
 }
 }
 else
@@ -63,22 +32,6 @@ else
 command = get_command(buff, size);
 words = argv(command);
 excut(words, envp);
-free(command);
-free(words);
 }
-path = _getenv("PATH");
-path_list = linkpath(path);
-filename = "file";
-full_path = _which(filename, path_list);
-if (full_path != NULL)
-{
-printf("Found executable at: %s\n", full_path);
-}
-else
-{
-printf("Executable not found in PATH\n");
-}
-free_list(path_list);
 return (0);
 }
-
