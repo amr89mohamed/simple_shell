969d615eae6298e0436ac670d3412f23955a2280
amr89moahmed <amr89mohamed@gmail.com>
2024-02-29 09:12:16 -0800

    its now in the same order

--- excute.c
+++ excute.c
@@ -6,8 +6,8 @@
  * to create a process to excute it
  */void excut(char **words, char **envp)
 {
-
 pid_t pid = fork();
+int pipefd[2];
 int i;
 if (pid == -1)
 {
@@ -25,6 +25,30 @@ exit(EXIT_FAILURE);
 else
 {
 wait(&i);
+if (strcmp(words[0], "/bin/ls") == 0)
+{
+char *args[] = {"/bin/ls", NULL};
+pid_t ls_pid = fork();
+if (ls_pid == -1)
+{
+perror("Fork failed error: ");
+exit(EXIT_FAILURE);
+}
+else if (ls_pid == 0)
+{
+dup2(fileno(stdout), fileno(stderr));
+
+pipe(pipefd);
+dup2(pipefd[1], fileno(stdout));
+close(pipefd[0]);
+close(pipefd[1]);
+execve(args[0], args, envp);
+}
+else
+{
+waitpid(ls_pid, NULL, 0);
+}
+}
 }
 for (i = 0; words[i]; i++)
 {
