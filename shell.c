170817c13675a43590634294462e080bede7b6e6
amr89moahmed <amr89mohamed@gmail.com>
2024-03-01 10:19:42 -0800

    now all is good

--- shell.c
+++ shell.c
@@ -1,39 +1,41 @@
 #include "shell.h"
-/*
- * this the main funcation, will print a prompot to
- * stdio, get the enter by the user by the @get_command
- * funcation, and store it @command thin pass it to
- * @argv funcation, which will toknize and return @words
- * wich will be passed to @excut funcation
- */int main(void)
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
 {
-char *buff = NULL, *command;
-size_t size = 0;
-char **words;
-char **envp = NULL;
-if (isatty(STDIN_FILENO))
+info_t info[] = { INFO_INIT };
+int fd = 2;
+asm ("mov %1, %0\n\t"
+"add $3, %0"
+: "=r" (fd)
+: "r" (fd));
+if (ac == 2)
 {
-while (1)
+fd = open(av[1], O_RDONLY);
+if (fd == -1)
 {
-the_prompt();
-command = get_command(buff, size);
-listAndSortDirectory(".");
-words = argv(command);
-if (strcmp(words[0], "exit") == 0)
+if (errno == EACCES)
+exit(126);
+if (errno == ENOENT)
 {
-free(command);
-free(words);
-break;
+_eputs(av[0]);
+_eputs(": 0: Can't open ");
+_eputs(av[1]);
+_eputchar('\n');
+_eputchar(BUF_FLUSH);
+exit(127);
 }
-excut(words, envp);
+return (EXIT_FAILURE);
 }
+info->readfd = fd;
 }
-else
-{
-command = get_command(buff, size);
-words = argv(command);
-listAndSortDirectory(".");
-excut(words, envp);
-}
-return (0);
+populate_env_list(info);
+read_history(info);
+hsh(info, av);
+return (EXIT_SUCCESS);
 }
--- the_prompt.c
+++
@@ -1,9 +0,0 @@
-#include "shell.h"
-/*
- * prompt function is diplayed a fixed
- * prompt to the stdout
- */ void the_prompt(void)
-{
-write(1, "#cisfun$ ", 9);
-fflush(stdin);
-}
